d93b32359c0bc2c9c080ab0f9bacb6b823e57d03
FelipeRibeiro <felipper43@gmail.com>
2024-01-04 21:00:01 -0300

    feat: add route get

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!, SSL certificate is working!');
 });
 
